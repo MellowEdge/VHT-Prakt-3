bfe4cf5ddbcd32f8a531d80db3a04a0b1851781c
Simon Boytcov <bsimon20@gmail.com>
2019-02-23 12:19:35 +0200

    First commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		helloWords(4);
+	}
+
+	private static void helloWords(int n) {
+		for (int i=0; i<n; i++) {
+			System.out.println("Hello world number "+i+"!");
+		}
+		
+	}
+}
